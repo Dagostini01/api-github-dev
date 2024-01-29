dd871675133e5b1a8e32e9585b935647aebf1723
Dagostini01 <pedro01dagostini@gmail.com>
2024-01-29 14:45:18 -0300

    mudanca nome MainPage

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Main from "./pages/Main";
+import Main from "./pages/MainPage";
 
 export function AppRoutes() {
   return (
