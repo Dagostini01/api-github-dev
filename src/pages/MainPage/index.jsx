29bf50d852ac8a030fe442d9d378212d08b0f77d
Dagostini01 <pedro01dagostini@gmail.com>
2024-01-30 15:52:26 -0300

    forms finalizado

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Container, Logo } from "./styles";
+import { Container, Logo, Title, Form, Input, Button } from "./styles";
 import githublogo from "../../assets/images/github-logo.svg";
+import { MdSearch } from 'react-icons/md';
 
 const MainPage = () => (
 
     <Container>
       <Logo src={githublogo} alt="API Github"/>
+      <Title>API Github</Title>
+      <Form>
+        <Input placeholder="usuário" />
+        <Button>
+          <MdSearch size={42} />
+        </Button>
+      </Form>
     </Container>
 
 )
--- src/pages/MainPage/styles.js
+++ src/pages/MainPage/styles.js
@@ -13,3 +13,46 @@ export const Container = styled.div`
 export const Logo = styled.img`
   width: 130px;
 `;
+
+export const Title = styled.h1`
+  font-size: 2.2rem;
+  color: ${(props) => props.theme.colors.white};
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 583px;
+`;
+
+export const Input = styled.input`
+  background: rgba(0, 0, 0, 0.8);
+  color: ${(props) => props.theme.colors.white};
+  width: 100%;
+  height: 64px;
+  line-height: 64px;
+  border: none;
+  font-size: 24px;
+  margin-right: 24px;
+  padding: 0 24px;
+  &::placeholder{
+    color: ${(props) => props.theme.colors.gray600};
+  }
+`;
+
+export const Button = styled.button`
+  color: ${(props) => props.theme.colors.white};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  width: 80px;
+  height: 64px;
+  border: 3px solid ${(props) => props.theme.colors.white};
+  transition: background 0.3s;
+  &:hover{
+    background: ${(props) => props.theme.colors.container};;
+  }
+`;
